795b4fa9f87babc600eb14f2b75cc95814de9ee7
Diego Nieto Cid <dnietoc@gmail.com>
2016-08-01 12:00:07 -0300

    Install workaround

--- server/main.js
+++ server/main.js
@@ -3,3 +3,11 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+// Temporary workaround for materialize fonts
+WebApp.connectHandlers.use("/packages/materialize_materialize/fonts", function (req, res) {
+	const url = req.originalUrl.replace("/fonts/", "/dist/fonts/");
+	res.statusCode = 301;
+	res.setHeader("Location", url);
+	res.end();
+});
